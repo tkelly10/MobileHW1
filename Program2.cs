bcb3e910dca7731adf637c37198cf81815471620
Tyler K <tkelly10@my.wctc.edu>
2020-02-03 21:36:18 -0600

    fixed it again

--- Program2.cs
+++ Program2.cs
@@ -19,63 +19,67 @@ namespace MobileHwProgram1_2
 
             do
             {
+                stillGoing = true;
                 Console.WriteLine("1) Student\n2) Teacher\n3) Exit");
                 input = Console.ReadLine();
-                stillGoing = true;
-                Console.WriteLine("Name: ");
-                Name = Console.ReadLine();
-                Console.WriteLine("Age: ");
-                Age = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("ID: ");
-                Id = Convert.ToInt32(Console.ReadLine());
+                if(input != "3")
+                {
+                    Console.WriteLine("Name: ");
+                    Name = Console.ReadLine();
+                    Console.WriteLine("Age: ");
+                    Age = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("ID: ");
+                    Id = Convert.ToInt32(Console.ReadLine());
 
-                foreach(SchoolProgram sp in Enum.GetValues(typeof(SchoolProgram)))
-                {
-                    Console.WriteLine($"{counter}) {sp}");
-                    counter++;
-                }
-                counter = 1;
-                int choice = Convert.ToInt32(Console.ReadLine());
-                foreach(SchoolProgram sp in Enum.GetValues(typeof(SchoolProgram)))
-                {
-                    if ((choice - 1) == (int)sp)
+                    foreach (SchoolProgram sp in Enum.GetValues(typeof(SchoolProgram)))
                     {
-                        Program = sp;
+                        Console.WriteLine($"{counter}) {sp}");
+                        counter++;
+                    }
+                    counter = 1;
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    foreach (SchoolProgram sp in Enum.GetValues(typeof(SchoolProgram)))
+                    {
+                        if ((choice - 1) == (int)sp)
+                        {
+                            Program = sp;
+                        }
+                    }
+
+                    switch (input)
+                    {
+                        case "1":
+                            {
+                                Student student = new Student
+                                {
+                                    Name = Name,
+                                    Age = Age,
+                                    Id = Id,
+                                    Program = Program
+                                };
+                                Console.WriteLine("Credits Earned: ");
+                                student.Credit = Convert.ToInt32(Console.ReadLine());
+                                People.Add(student);
+                                break;
+                            }
+                        case "2":
+                            {
+                                Teacher teacher = new Teacher
+                                {
+                                    Name = Name,
+                                    Age = Age,
+                                    Id = Id,
+                                    Program = Program
+                                };
+                                Console.WriteLine("Years of Service");
+                                teacher.Tenure = Convert.ToInt32(Console.ReadLine());
+                                People.Add(teacher);
+                                break;
+                            }
+                        case "3": { stillGoing = false; break; }
                     }
                 }
                 
-                switch (input)
-                {
-                    case "1":
-                        {
-                            Student student = new Student
-                            {
-                                Name = Name,
-                                Age = Age,
-                                Id = Id,
-                                Program = Program
-                            };
-                            Console.WriteLine("Credits Earned: ");
-                            student.Credit = Convert.ToInt32(Console.ReadLine());
-                            People.Add(student);
-                            break;
-                        }
-                    case "2":
-                        {
-                            Teacher teacher = new Teacher
-                            {
-                                Name = Name,
-                                Age = Age,
-                                Id = Id,
-                                Program = Program
-                            };
-                            Console.WriteLine("Years of Service");
-                            teacher.Tenure = Convert.ToInt32(Console.ReadLine());
-                            People.Add(teacher);
-                            break;
-                        }
-                    case "3": { stillGoing = false; break; }
-                }
             } while (stillGoing);
 
             foreach(Person p in People)
