aa620a4fe7ce04581d9dc5bbd480c31c01927c9c
Tyler K <tkelly10@my.wctc.edu>
2020-02-03 19:46:56 -0600

    same as last time

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MobileHwProgram1_2
         public static void Main(string[] args)
         {
 
-            //Program1 pro1 = new Program1();
+            Program1 pro1 = new Program1();
             Program2 pro2 = new Program2();
 
         }
